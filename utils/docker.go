ad243117cab2b2127673cfb7fd6f1cc0d63d2dae
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-06-10 13:52:01 +0000

    hope

--- utils/docker.go
+++ utils/docker.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"time"
 )
@@ -21,6 +22,17 @@ func DockerUpdate(version string) bool {
 	time.Sleep(1 * time.Second)
 	printLogs(exec.Command("docker", "stop", containerName))
 	time.Sleep(1 * time.Second)
-	printLogs(exec.Command("docker", "start", containerName))
+	// remove container
+	printLogs(exec.Command("docker", "rm", containerName))
+	time.Sleep(1 * time.Second)
+	// get enviroment variables\
+	envValues := os.Environ()
+	var env string = ""
+	for _, value := range envValues {
+		env += "-e " + value + " "
+	}
+	// create container depand on database
+	fmt.Println("docker", "run", "-d", "--name", containerName, "--network", "probat", env, "n4z3m/probat-fstack:latest")
+	printLogs(exec.Command("docker", "run", "-d", "--name", containerName, "--network", "probat", env, "n4z3m/probat-fstack:latest"))
 	return true
 }
