98493891ae9f5f65aeed358ec9f4267692739d98
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-06-10 05:45:42 +0000

    yep

--- utils/docker.go
+++ utils/docker.go
@@ -7,8 +7,8 @@ func DockerUpdate(version string) {
 	image := GetEnv("DOCKER_IMAGE") + ":" + version
 	cmd := "docker-compose pull " + image
 	exec.Command("sh", "-c", cmd).Run()
-	stopCmd := "docker-compose stop " + image
+	stopCmd := "docker-compose stop backend" + image
 	exec.Command("sh", "-c", stopCmd).Run()
-	upCmd := "docker-compose up -d " + image
+	upCmd := "docker-compose up -d backend" + image
 	exec.Command("sh", "-c", upCmd).Run()
 }
