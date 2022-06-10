4b481b01e8e603b4237a65af95166e66db560315
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-06-10 11:04:12 +0000

    new changes

--- utils/docker.go
+++ utils/docker.go
@@ -18,8 +18,9 @@ func DockerUpdate(version string) bool {
 	containerName := "probat_backend"
 	// cmd := "docker-compose pull " + containerName
 	printLogs(exec.Command("docker", "pull", "n4z3m/probat-fstack:latest"))
-	time.Sleep(time.Minute)
+	time.Sleep(5 * time.Second)
 	printLogs(exec.Command("docker", "stop", containerName))
+	time.Sleep(5 * time.Second)
 	printLogs(exec.Command("docker", "start", containerName))
 	return true
 }
