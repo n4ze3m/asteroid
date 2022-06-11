090f22ccff36dc43f18ff439723fd0a3b2ade99a
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-06-11 08:03:30 +0000

    bug added

--- utils/docker.go
+++ utils/docker.go
@@ -7,16 +7,18 @@ import (
 )
 
 func printLogs(cmd *exec.Cmd) {
+	fmt.Println(cmd.Args)
 	out, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err)
 	}
+	// get the output
 	fmt.Println(string(out))
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 5)
 }
 
 func DockerUpdate(version string) bool {
-	printLogs(exec.Command("docker-compose", "pull", "backend"))
+	printLogs(exec.Command("docker", "pull", "n4z3m/probat-fstack:latest"))
 	printLogs(exec.Command("docker-compose", "stop", "backend"))
 	printLogs(exec.Command("docker-compose", "start", "backend"))
 	return true
