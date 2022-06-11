942d1e38b030bde8772b0a7f125e6919a1262d04
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-06-11 07:05:15 +0000

    new action

--- utils/docker.go
+++ utils/docker.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"time"
 )
@@ -12,27 +11,13 @@ func printLogs(cmd *exec.Cmd) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("response", string(out))
+	fmt.Println(string(out))
+	time.Sleep(time.Second * 2)
 }
 
 func DockerUpdate(version string) bool {
-	containerName := "probat_backend"
-	// cmd := "docker-compose pull " + containerName
-	printLogs(exec.Command("docker", "pull", "n4z3m/probat-fstack:latest"))
-	time.Sleep(1 * time.Second)
-	printLogs(exec.Command("docker", "stop", containerName))
-	time.Sleep(1 * time.Second)
-	// remove container
-	printLogs(exec.Command("docker", "rm", containerName))
-	time.Sleep(1 * time.Second)
-	// get enviroment variables\
-	envValues := os.Environ()
-	var env string = ""
-	for _, value := range envValues {
-		env += "-e " + value + " "
-	}
-	// create container depand on database
-	fmt.Println("docker", "run", "-d", "--name", containerName, "--network", "probat", env, "n4z3m/probat-fstack:latest")
-	printLogs(exec.Command("docker", "run", "-d", "--name", containerName, "--network", "probat", env, "-p", "7362:8000", "n4z3m/probat-fstack:latest"))
+	printLogs(exec.Command("docker-compose", "pull", "bcakend"))
+	printLogs(exec.Command("docker-compose", "stop", "bcakend"))
+	printLogs(exec.Command("docker-compose", "start", "bcakend"))
 	return true
 }
